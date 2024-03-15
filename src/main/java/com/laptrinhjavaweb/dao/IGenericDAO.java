ae7b2debdd052be8806d7f4df48dbb1b686d390f
Admin <Admin@-PC>
2024-03-15 19:30:21 +0700

    create IGenericDAO, new method in AbstractDAO [query(sql,rowMapper,parameters)]

---
+++ src/main/java/com/laptrinhjavaweb/dao/IGenericDAO.java
@@ -0,0 +1,20 @@
+package com.laptrinhjavaweb.dao;
+
+import java.util.List;
+
+import com.laptrinhjavaweb.mapper.RowMapper;
+
+public interface IGenericDAO<T> {
+	/*
+	 * 
+	 * hàm chung có 3 đối tượng truyền vào hàm: 
+	 * - câu query (sql)
+	 * - đối tượng cần trả về (T)
+	 * - giá trị của tham số (Object... parameters)
+	 * 
+	 * */
+	@SuppressWarnings("hiding")
+	// Object... == Object[]
+	public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters);	
+	
+}
--- src/main/java/com/laptrinhjavaweb/dao/impl/CategoryDAO.java
+++ src/main/java/com/laptrinhjavaweb/dao/impl/CategoryDAO.java
@@ -1,7 +1,6 @@
 package com.laptrinhjavaweb.dao.impl;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,62 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.laptrinhjavaweb.dao.ICategoryDAO;
+import com.laptrinhjavaweb.mapper.CategoryMapper;
 import com.laptrinhjavaweb.model.CategoryModel;
 
-public class CategoryDAO implements ICategoryDAO {
+public class CategoryDAO extends AbstractDAO<CategoryModel> implements ICategoryDAO {
 	
-	public Connection getConnection() {
-		// ClassNotFoundException: prevent when we forget add dependency in pom.xml
-		// SQLException: url | user | password is not correct
-		try {
-			Class.forName("com.mysql.jdbc.Driver");			// load Driver (giống cổng kết nối | Connect vô mysql)
-			String url = "jdbc:mysql://localhost:3306/newservlet";
-			String user = "root";
-			String password = "123456";
-			return DriverManager.getConnection(url, user, password);
-		} catch (ClassNotFoundException | SQLException e) {		// multiple catch (2 case) 
-			return null;
-		} /*catch (SQLException e) {
-			return null;
-		}*/
-	}
-
 	@Override
 	public List<CategoryModel> findAll() {
-		List<CategoryModel> results = new ArrayList<>();
-		// open connection 
 		String sql = "SELECT * FROM newservlet.category";
-		Connection connection = getConnection();
-		PreparedStatement statement = null;		// Truyền câu query vào
-		ResultSet resultSet = null;				// Đối tượng được trả về sau khi query data lên
-		if (connection != null) {
-			try {
-				statement = connection.prepareStatement(sql);
-				// init parameter when have WHERE ; ONLY can did it at PreparedStatement [not Statement]
-				resultSet = statement.executeQuery();	// execute query => return object resultSet
-				while(resultSet.next()) {
-					CategoryModel category = new CategoryModel();
-					category.setId(resultSet.getLong("id"));
-					category.setName(resultSet.getString("name"));
-					category.setCode(resultSet.getString("code"));
-					results.add(category);
-				}
-				return results;
-			} catch (SQLException e) {
-				return null;
-			} finally {			// Luôn đóng lại tất cả đối tượng (tác vụ) - cả trong trường hợp xấu nhất là bị lỗi ở các command trên
-				try {
-					if (connection != null) 
-						connection.close();	
-					if (statement != null) 
-						statement.close();
-					if (resultSet != null)
-						resultSet.close();
-				} catch (SQLException e) {
-					return null;
-				}
-			}
-		}
-		return results;
+		return query(sql, new CategoryMapper());
 	}
 }
