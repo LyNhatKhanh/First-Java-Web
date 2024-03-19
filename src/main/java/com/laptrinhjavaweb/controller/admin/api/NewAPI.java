3a22b886bd592351a9d943cb279835030201f6f5
LyNhatKhanh <lynhatkhanh.2001@gmail.com>
2024-03-19 22:29:01 +0700

    - create NewAPI access database to INSERT INTO, UPDATE, DELETE

---
+++ src/main/java/com/laptrinhjavaweb/controller/admin/api/NewAPI.java
@@ -0,0 +1,73 @@
+package com.laptrinhjavaweb.controller.admin.api;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laptrinhjavaweb.model.NewModel;
+import com.laptrinhjavaweb.service.INewService;
+import com.laptrinhjavaweb.utils.HttpUtil;
+
+@WebServlet(urlPatterns = {"/api-admin-new"})
+public class NewAPI extends HttpServlet {
+
+	private static final long serialVersionUID = -915988021506484384L;
+	
+	@Inject
+	private INewService newService;
+	
+	// *** ADD ***
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// response to client
+		ObjectMapper mapper = new ObjectMapper();
+		// Servlet code
+		request.setCharacterEncoding("UTF-8");			// chap nhan font VN nhan tu client
+		response.setContentType("application/json");	// KDL khi tra KQ lai Client
+		// binding JSON to NewModel : HttpUtil.of(request.getReader()) : simple Object of HttpUtils
+		NewModel newModel = HttpUtil.of(request.getReader()).toModel(NewModel.class); 
+		newModel = newService.save(newModel);
+		mapper.writeValue(response.getOutputStream(), newModel);
+	}
+	
+	// *** UPDATE ***
+	public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// response to client
+		ObjectMapper mapper = new ObjectMapper();
+		// Servlet code
+		request.setCharacterEncoding("UTF-8"); // chap nhan font VN nhan tu client
+		response.setContentType("application/json"); // KDL khi tra KQ lai Client
+		// binding JSON to NewModel : HttpUtil.of(request.getReader()) : simple Object of HttpUtils
+		NewModel updateNew = HttpUtil.of(request.getReader()).toModel(NewModel.class);
+		updateNew = newService.update(updateNew); 
+		mapper.writeValue(response.getOutputStream(), updateNew);
+	}
+	
+	// *** DELETE ***
+	public void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// response to client
+		ObjectMapper mapper = new ObjectMapper();
+		// Servlet code
+		request.setCharacterEncoding("UTF-8"); // chap nhan font VN nhan tu client
+		response.setContentType("application/json"); // KDL khi tra KQ lai Client
+		// binding JSON to NewModel : HttpUtil.of(request.getReader()) : simple Object of HttpUtils
+		NewModel newModel = HttpUtil.of(request.getReader()).toModel(NewModel.class);
+		newService.delete(newModel.getIds());
+		mapper.writeValue(response.getOutputStream(), "{}");
+	}
+	
+	/*// *** SELECT ***
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// Servlet code
+	}*/
+
+	
+}
---
+++ src/main/java/com/laptrinhjavaweb/utils/HttpUtil.java
@@ -0,0 +1,41 @@
+package com.laptrinhjavaweb.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class HttpUtil {
+	private String value;
+
+	public HttpUtil(String value) {
+		this.value = value;
+	}
+	
+	// String to Model
+	public <T> T toModel(Class<T> tClass) {	// can truyen class vao => model.class
+		try {
+			// String => Model
+			return new ObjectMapper().readValue(value, tClass); // value: lay gia tri tu method of(reader)
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+
+	// JSON to String
+	public static HttpUtil of(BufferedReader reader) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			String line;
+			while ((line = reader.readLine()) != null)
+				sb.append(line);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return new HttpUtil(sb.toString());
+	}
+	
+	
+}
